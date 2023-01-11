a786317636f5e744680ccefc33f00729485e30f3
matsutake-taketo <120348208+matsutake-taketo@users.noreply.github.com>
2023-01-11 09:48:04 +0900

    hennsuu
    5

--- GitTest/test01/ソース.cpp
+++ GitTest/test01/ソース.cpp
@@ -2,7 +2,10 @@
 
 int main(void)
 {
+	int num = 5;
+	printf("%d\n", num);
 	printf("Hello Git World!!\n");
+
 	printf("Hello Git World!!\n");
 	printf("Hello Git World!!\n");
 	printf("Hello Git World!!\n");
